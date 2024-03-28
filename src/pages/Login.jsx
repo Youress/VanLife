99af0916d5fbf0de2810d486115744aaad1b4a17
Youssef Essouairi <rajawiiyoussef@gmail.com>
2024-03-28 15:52:35 +0000

    Login Form react router

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,72 +1,62 @@
 import React, { useState } from "react";
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, Form, redirect } from "react-router-dom";
 import { loginUser } from "../api";
 
 export function loader({ request }) {
   return new URL(request.url).searchParams.get("message");
 }
-
+export async function action({ request }) {
+  const formdata = await request.formData();
+  const email = formdata.get("email");
+  const password = formdata.get("password");
+  const data = await loginUser({ email, password });
+  localStorage.setItem("loggedin", true);
+  return redirect("/host")
+}
 export default function Login() {
   const message = useLoaderData();
   const [status, setStatus] = useState("idle");
-  const [error , setError] = useState(null)
+  const [error, setError] = useState(null);
   const [loginFormData, setLoginFormData] = useState({
     email: "",
     password: "",
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     setStatus("submitting");
     e.preventDefault();
     loginUser(loginFormData)
-      .then(() => navigate('/host' , {relative : true}))
-      .catch(error => setError(error))
+      .then(() => navigate("/host", { replace: true }))
+      .catch((error) => setError(error))
       .finally(() => setStatus("idle"));
-  }
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setLoginFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
   }
 
   return (
     <div className="flex flex-col items-center px-6 ">
       <h1 className="font-bold text-3xl my-3">Sign in to your account</h1>
-      <form
-        onSubmit={handleSubmit}
+      <Form
+        method="post"
         className="flex flex-col w-full max-w-[500px] login-form text-center"
       >
         {message && (
           <h2 className="text-2xl text-red-600 mb-2 font-bold">{message}</h2>
         )}
         {error && (
-          <h2 className="text-2xl text-red-600 mb-2 font-bold">{error.message}</h2>
+          <h2 className="text-2xl text-red-600 mb-2 font-bold">
+            {error.message}
+          </h2>
         )}
 
-        <input
-          name="email"
-          onChange={handleChange}
-          type="email"
-          placeholder="Email address"
-          value={loginFormData.email}
-        />
-        <input
-          name="password"
-          onChange={handleChange}
-          type="password"
-          placeholder="Password"
-          value={loginFormData.password}
-        />
+        <input name="email" type="email" placeholder="Email address" />
+        <input name="password" type="password" placeholder="Password" />
         <button
           className="bg-[#FF8C38] w-full h-14 mt-3 text-white rounded-md"
-          disabled={status === 'submitting'}
-        >{status === 'submitting' ? 'Logging in ...' : 'Log in'}
+          disabled={status === "submitting"}
+        >
+          {status === "submitting" ? "Logging in ..." : "Log in"}
         </button>
-      </form>
+      </Form>
     </div>
   );
 }
